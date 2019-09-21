cd8314c9e40412a06e829c3b324ae318eb05d3e7
Constantin Ross <contact@constantinross.com>
2019-09-21 19:03:03 +0200

    removed lodash parameter and changed version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const importComponents = (_, Vue, components) => {
+const importComponents = (Vue, components) => {
   components.keys().forEach((fileName) => {
     const componentConfig = components(fileName);
     const componentName = fileName.split('/').pop().split('.')[0];
